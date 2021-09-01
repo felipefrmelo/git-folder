77d38ae6e3bdd696d041500f1564f9e6f7bda3e8
felipe <felipefrmelo@gmail.com>
2021-09-01 12:53:26 -0300

    add readme

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+lein uberjar
 cat stub.sh target/uberjar/git-folder-0.1.0-SNAPSHOT-standalone.jar > git-folder && chmod +x git-folder 
--- test/git_folder/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns git-folder.core-test
-  (:require [clojure.test :refer :all]
-            [git-folder.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
