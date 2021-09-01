0d3daacd9ea05529a5031c0d287f4e833191f7cd
felipe <felipefrmelo@gmail.com>
2021-09-01 12:40:53 -0300

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to git-folder
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat stub.sh target/uberjar/git-folder-0.1.0-SNAPSHOT-standalone.jar > git-folder && chmod +x git-folder 
